3c0ff9252bbca9da40c9dcd62ea9a3f0b97e5576
Lucas Viana <lucas_viana99@outlook.com>
2021-12-22 01:13:33 -0300

    A leitura do arquivo CSV está sendo feita corretamente.

---
+++ CSVRead/Atendimento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CsvHelper.Configuration.Attributes;
+
+namespace ArtigoCsv
+{
+    public class Atendimento
+    {
+        [Name("id")]
+        public int Id { get; set; }
+        [Name("nome_clinica")]
+        public string NomeClinica { get; set; }
+        [Name("nome_paciente")]
+        public string NomePaciente { get; set; }
+        [Name("data_nascimento")]
+        public DateTime DataNascimento { get; set; }
+        [Name("data_atendimento")]
+        public DateTime DataAtendimento { get; set; }
+        [Name("especialidade")]
+        public string Especialidade { get; set; }
+    }
+}
---
+++ CSVRead/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace ArtigoCsv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+            };
+
+            using (var reader = new StreamReader("/Users/lucas/Desktop/CSV/Clinica.csv"))
+            using (var csv = new CsvReader(reader, config))
+            {
+                var atendimentos = csv.GetRecords<Atendimento>();
+
+                foreach (var atendimento in atendimentos)
+                    Console.WriteLine($"Paciente: {atendimento.NomePaciente}, Clínica: {atendimento.NomeClinica}");
+            }
+        }
+    }
+}
