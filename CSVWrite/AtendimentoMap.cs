d6f66e50eb74fd319a78b1c2b64cdc353b26a21d
Lucas Viana <lucas_viana99@outlook.com>
2021-12-22 01:31:33 -0300

    Teste realizado na escrita do arquivo CSV. Feito com sucesso! Mini Projeto Finalizado

---
+++ CSVWrite/AtendimentoMap.cs
@@ -0,0 +1,17 @@
+﻿using CsvHelper.Configuration;
+
+namespace ArtigoCsv
+{
+    public class AtendimentoMap : ClassMap<Atendimento>
+    {
+        public AtendimentoMap()
+        {
+            Map(m => m.Id).Name("id");
+            Map(m => m.NomeClinica).Name("nome_clinica");
+            Map(m => m.NomePaciente).Name("nome_paciente");
+            Map(m => m.DataNascimento).Name("data_nascimento");
+            Map(m => m.DataAtendimento).Name("data_atendimento");
+            Map(m => m.Especialidade).Name("especialidade");
+        }
+    }
+}
--- CSVWrite/Program.cs
+++ CSVWrite/Program.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 
-namespace CSVWrite
+namespace ArtigoCsv
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+            };
+
+            List<Atendimento> atendimentos;
+
+            using (var reader = new StreamReader("Clinica.csv"))
+            using (var csv = new CsvReader(reader, config))
+            {
+                csv.Context.RegisterClassMap<AtendimentoMap>();
+
+                atendimentos = csv.GetRecords<Atendimento>().ToList();
+
+                foreach (var atendimento in atendimentos)
+                    Console.WriteLine($"Paciente: {atendimento.NomePaciente}, Clínica: {atendimento.NomeClinica}");
+            }
+
+            using (var writer = new StreamWriter("Clinica.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.Context.RegisterClassMap<AtendimentoMap>();
+
+                csv.WriteHeader<Atendimento>();
+                csv.NextRecord();
+
+                csv.WriteRecords(atendimentos);
+            }
         }
     }
 }
